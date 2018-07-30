ea14cd0a52df9ff6a1257534ccaa72cd24958d59
Benjamin Pradon <pradonbenjamin@gmail.com>
2018-07-31 00:14:33 +0200

    updates on readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Financial_python_project
+
+As I'm learning Python and I have registered the feed-back a recruiter gave me to put forward my "management work", I'm going trough a fiancial python project.
--- financial app.py
+++ financial app.py
@@ -7,12 +7,14 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2000,1,1)
-end = dt.datetime.now()
-df = web.DataReader('GOOGL', 'morningstar', start, end)
+#start = dt.datetime(2000,1,1)
+#end = dt.datetime.now()
+#df = web.DataReader('GOOGL', 'morningstar', start, end)
 #df.to_csv('google.csv')
-df = pd.read_csv('google.csv', parse_dates=True, index_col=0)
-print(df.head())
+df = pd.read_csv('google.csv', parse_dates=True, index_col=0)#reads on sql, excel...
+#print(df[['Open', 'High']].head())
 
-df['Open'].plot()
-plt.show()
+#df.plot()
+#plt.show()
+
+df['100ma'] =df['High'].rolling(window=100).mean() # creating a colomn 100 moving average
